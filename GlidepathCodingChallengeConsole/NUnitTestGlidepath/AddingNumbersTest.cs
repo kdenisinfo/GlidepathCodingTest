a83082719a27eadc0a22b36452713817a981a2e4
Kovalenko Denis <kdenisinfo@gmail.com>
2021-09-24 10:51:33 +1200

    changes to the Adding Numbers unit tests

--- GlidepathCodingChallengeConsole/NUnitTestGlidepath/AddingNumbersTest.cs
+++ GlidepathCodingChallengeConsole/NUnitTestGlidepath/AddingNumbersTest.cs
@@ -19,7 +19,7 @@ namespace Tests
         }
 
         [Test]
-        public void AddingNumbers_TotalNumbers_Colculations()
+        public void Adding_Numbers_Total_Numbers_Checkins_LessThan152()
         {
             // Arrange
             var addingnumbers = new AddingNumbers(6 , 7);
@@ -32,7 +32,7 @@ namespace Tests
         }
 
         [Test]
-        public void AddingNumbers_TotalNumbers_Calculations_MoreThan152()
+        public void Adding_Numbers_Total_Numbers_Chekins_MoreThan152()
         {
             // Arrange
             var addingnumbers = new AddingNumbers(78, 137);
@@ -42,6 +42,45 @@ namespace Tests
 
             //Assert
             Assert.AreEqual(63, totalnumber);
+        }
+
+       [Test]
+        public void Adding_Numbers_Total_Numbers_Chekins_EqualTo152()
+        {
+            // Arrange
+            var addingnumbers = new AddingNumbers(100, 52);
+
+            // Act
+            var totalnumber = addingnumbers.TotalNumber;
+
+            //Assert
+            Assert.AreEqual(152, totalnumber);
+        }
+
+        [Test]
+        public void Adding_Numbers_Total_Numbers_Chekins_Negative()
+        {
+            // Arrange
+            var addingnumbers = new AddingNumbers(-100, 52);
+
+            // Act
+            var totalnumber = addingnumbers.TotalNumber;
+
+            //Assert
+            Assert.AreEqual(-48, totalnumber);
+        }
+
+        [Test]
+        public void Adding_Numbers_Total_Numbers_Chekins_Negative_Both()
+        {
+            // Arrange
+            var addingnumbers = new AddingNumbers(-100, -52);
+
+            // Act
+            var totalnumber = addingnumbers.TotalNumber;
+
+            //Assert
+            Assert.AreEqual(-152, totalnumber);
         }
     }
 }
