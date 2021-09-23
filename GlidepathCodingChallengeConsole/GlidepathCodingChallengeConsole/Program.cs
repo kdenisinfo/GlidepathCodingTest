c50fc2405dfc7f956f3cd726bf0db4a0cab405cb
Kovalenko Denis <kdenisinfo@gmail.com>
2021-09-23 19:29:22 +1200

    initial commit

---
+++ GlidepathCodingChallengeConsole/GlidepathCodingChallengeConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlidepathCodingChallengeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
